36c2cb95abf6bbd4f196466e58e296e0b07abbd4
Jan Haug <jan.haug@digitecgalaxus.ch>
2026-07-22 16:13:08 +0200

    Register login theme as public Umbraco manifest via IPackageManifestReader

---
+++ Infrastructure/Shared/LoginThemeManifest.cs
@@ -0,0 +1,38 @@
+using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Manifest;
+
+namespace RedAnts.Infrastructure.Shared;
+
+public sealed class LoginThemeManifestReader : IPackageManifestReader
+{
+    public Task<IEnumerable<PackageManifest>> ReadPackageManifestsAsync()
+    {
+        var manifest = new PackageManifest
+        {
+            Name = "RedAnts.LoginTheme",
+            AllowPublicAccess = true,
+            Extensions =
+            [
+                new
+                {
+                    type = "appEntryPoint",
+                    alias = "RedAnts.Login.Theme",
+                    name = "Red Ants Login Theme",
+                    js = "/App_Plugins/RedAntsLogin/login-theme.js"
+                }
+            ]
+        };
+
+        return Task.FromResult<IEnumerable<PackageManifest>>(new[] { manifest });
+    }
+}
+
+public sealed class LoginThemeComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.Services.AddSingleton<IPackageManifestReader, LoginThemeManifestReader>();
+    }
+}
